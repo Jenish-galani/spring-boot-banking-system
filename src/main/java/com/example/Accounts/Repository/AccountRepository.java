9e40465e82f368da1da5c30ba9f9fe6ba9191470
Jenish Galani <jenishgalani4543@gmail.com>
2025-01-03 17:30:47 +0530

    Update AccountRepository.java
    Add Comment

--- src/main/java/com/example/Accounts/Repository/AccountRepository.java
+++ src/main/java/com/example/Accounts/Repository/AccountRepository.java
@@ -4,6 +4,8 @@ import com.example.Accounts.Model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    
+    // Finds an account by its unique account number.
     Account findByAccountNumber(String accountNumber);
 }
 
